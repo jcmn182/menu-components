d55458ae2e69f2f41e11148291ffbd8b3f7b9ae8
juan carlos <maciasnajeraj@gmail.com>
2021-03-18 21:53:50 -0600

    index controller 1

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,20 +1,19 @@
 import view from '../views/template.html';
 
-const getData = (data) => {
-    
-    const viewElement = document.createElement('div');
-    viewElement.innerHTML = view
-    const templateItem = viewElement.querySelector('#items-template').content
-    const fragment = document.createDocumentFragment();
+const viewElement = document.createElement('div');
+viewElement.innerHTML = view
+const templateItem = viewElement.querySelector('#items-template').content
+const fragment = document.createDocumentFragment();
 
-    data.forEach(data =>{
-        templateItem.querySelector('h5').textContent = data.name;
-        templateItem.querySelectorAll('p')[0].textContent = data.descripcion;
-        templateItem.querySelectorAll('p')[2].textContent = data.precio;
-        const clone = templateItem.cloneNode(true);
-        fragment.appendChild(clone)
-  })
-  return fragment
+
+export default   (data) =>{
+   
+   data.forEach(data =>{
+      templateItem.querySelector('h5').textContent = data.name;
+      templateItem.querySelectorAll('p')[0].textContent = data.descripcion;
+      templateItem.querySelectorAll('p')[2].textContent = data.precio;
+      const clone = templateItem.cloneNode(true);
+      fragment.appendChild(clone)
+})
+   return fragment
 }
-
-export {getData}
--- src/router/router.js
+++ src/router/router.js
@@ -5,17 +5,17 @@ const router = (route) =>{
         content.innerHTML = '';
     switch (route){
         case '#Premium':
-            return content.appendChild(options.premium);
+            return content.appendChild(options.premium());
         case '#Especiales':
-            return content.appendChild(options.especiales);
+            return content.appendChild(options.especiales());
         case '#Clasicas':
-                return content.appendChild(options.clasicas); 
+                return content.appendChild(options.clasicas()); 
         case '#Hot_Dogs':
-                return content.appendChild(options.hotdogs);
+                return content.appendChild(options.hotdogs());
         case '#Snacks':
-                return content.appendChild(options.snacks);
+                return content.appendChild(options.snacks());
         case '#Paquetes':
-                return content.appendChild(options.paquetes);
+                return content.appendChild(options.paquetes());
         default:
             return console.log('404 not found')                                  
     }
